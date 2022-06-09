ca888e83a0cb5b2efc386f11dcdae0ccca3cb426
namratasiv <namrata.sivakumar1@gmail.com>
2022-06-09 23:51:52 +0530

    random :/

--- weatherapp/settings.py
+++ weatherapp/settings.py
@@ -98,9 +98,10 @@ WSGI_APPLICATION = 'weatherapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'weather',
+        'NAME': 'weather'
     }
 }
+
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
